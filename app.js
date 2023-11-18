5dd63de3dbfd0ffea8d5d1acb6d8861cc94d6fb4
Dylan Marin <dylanjmmarin@gmail.com>
2023-11-18 13:39:03 -0500

    4.3 modules done

--- app.js
+++ app.js
@@ -2,18 +2,18 @@ import express from "express";
 import Hello from "./hello.js";
 import Lab5 from "./lab5.js";
 import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from "./modules/routes.js";
 
 
-// import ModuleRoutes from "./modules/routes.js";
 import cors from "cors";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// ModuleRoutes(app);
 
 CourseRoutes(app);
+ModuleRoutes(app);
 Lab5(app);
 Hello(app);
 

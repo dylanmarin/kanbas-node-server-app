87a23d04b8ad32be680866ebd9ef20ef9795bb3b
Dylan Marin <dylanjmmarin@gmail.com>
2023-11-28 19:58:59 -0500

    session impld

--- app.js
+++ app.js
@@ -4,17 +4,43 @@ import Hello from "./hello.js";
 import Lab5 from "./lab5.js";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
-
-
 import cors from "cors";
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
+import session from "express-session";
+import "dotenv/config";
+
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 
 const app = express();
-app.use(cors());
-app.use(express.json());
+app.use(
+    cors({
+        credentials: true,
+        origin: process.env.FRONTEND_URL
+    })
+);
 
+
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+    };
+}
+app.use(session(sessionOptions));
+
+app.use(express.json());
 
 CourseRoutes(app);
 ModuleRoutes(app);
+UserRoutes(app);
+
 Lab5(app);
 Hello(app);
 
